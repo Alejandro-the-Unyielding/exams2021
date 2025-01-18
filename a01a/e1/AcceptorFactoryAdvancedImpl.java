6289dbc4cf7f495d371dd4879967b65e9079f54b
Daniel Alejandro <d.alejandrohorna@gmail.com>
2025-01-18 03:09:57 +0100

    tiny suppress mod. GUI still needs revision

--- a01a/e1/AcceptorFactoryAdvancedImpl.java
+++ a01a/e1/AcceptorFactoryAdvancedImpl.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
+@SuppressWarnings("unused")
 public class AcceptorFactoryAdvancedImpl implements AcceptorFactory{
 
     @Override
